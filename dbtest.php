9920a2c9997e6f7569f82172975b730f2deb775f
root <root@master.example.com>
2019-01-20 21:19:15 +0530

    first commit

---
+++ dbtest.php
@@ -0,0 +1,24 @@
+<?php
+$dbhost = getenv("DB_SERVICE_HOST");
+$dbport = getenv("DB_SERVICE_PORT");
+$dbuser = getenv("DB_USER");
+$dbname = getenv("DB_NAME");
+$dbpwd = getenv("DB_PASSWORD");;
+ 
+
+
+$connection = mysqli_connect($dbhost.":".$dbport, $dbuser, $dbpwd, $dbname) or die("Error " . mysqli_error($connection));
+
+
+$query = "SELECT * from users" or die("Error in the consult.." . mysqli_error($connection));
+
+echo "Hello All.. Here is the list of users: <br>";
+$rs = $connection->query($query);
+while ($row = mysqli_fetch_assoc($rs)) {
+    echo "User Id: ".$row['user_id'] . " User Name: " . $row['username'] . "<br>";
+}
+echo "End of the list <br>";
+
+mysqli_close($connection);
+
+?>
